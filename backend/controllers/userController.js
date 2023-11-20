56dd652af53db1260e68218d646b9f0bdddfd038
Zakaria <zaknetsum@gmail.com>
2023-11-20 20:38:16 +0600

    first commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1799,7 +1799,7 @@ exports.addParent4And5 = catchAsyncErrors(async (req, res, next) => {
 });
 
 // every 1 minute corn job
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('0 * * * *', async () => {
 	// get all active users
 	const users = await User.find({ is_active: true, rank_is_processing: false });
 	if (!users) {
